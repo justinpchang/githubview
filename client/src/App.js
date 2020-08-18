eeb85ec0511d670e4f12b4e80a144ee5cc72404d
Justin Chang <justin.p.chang@gmail.com>
2020-08-18 12:17:55 -0400

    Add basic client and server with communication

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    apiResponse: '',
+  };
+
+  callAPI = () =>
+    fetch('http://localhost:3001/testAPI')
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <p className='App-intro'>{this.state.apiResponse}</p>
+      </div>
+    )
+  }
+}
+
+export default App;
