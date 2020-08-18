54e374629bb35ed9135bfcbef0900773507e57f5
Julian Hamelberg <jshx@mit.edu>
2020-08-18 15:26:19 -0400

    long github url to small url

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('API is working properly');
+    res.send('API is working properly maybe? this is meant to be testAPI');
 });
 
 module.exports = router;
