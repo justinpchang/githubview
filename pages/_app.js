504b0067373e8f3b170b57e2e2294a1b1729955f
Justin Chang <justinpchang>
2021-08-10 17:04:53 -0400

    Add non-state github login at home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import { Provider } from 'next-auth/client';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 export default function Home() {
+    const [session, loading] = useSession();
+
+    console.log(session);
+
     return (
         <div className="container position-relative">
             <h1 className="display-3 text-center mt-5">
@@ -8,6 +14,16 @@ export default function Home() {
                 Navigate to a GitHub repo and replace the base URL with{' '}
                 <pre>githubview.com</pre>
             </p>
+            <br />
+            {!session ? <>
+                <h5 className="h5">Not signed in</h5>
+                <br />
+                <button className="btn btn-primary" onClick={() => signIn()}>Sign in</button>
+            </> : <>
+                <h5 className="h5">Signed in as {session.user.name}</h5>
+                <br />
+                <button className="btn btn-primary" onClick={() => signOut()}>Sign out</button>
+            </>}
         </div>
     );
 }
