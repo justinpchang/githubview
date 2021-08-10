12f79dc71592661da52bd00b747d8b00eeed06b2
Justin Chang <justinpchang>
2021-08-10 16:33:20 -0400

    Add mock data for file browser for styling

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -24,6 +24,8 @@ const files = [
 ];
 */
 
+const mockTree = [{"key":".eslintrc"},{"key":".gitignore"},{"key":".prettierrc"},{"key":"LICENSE"},{"key":"README.md"},{"key":"components/Browser/index.js"},{"key":"components/Browser/renderers/BrowserFile.js"},{"key":"components/Browser/renderers/BrowserFolder.js"},{"key":"components/Browser/renderers/BrowserHeader.js"},{"key":"components/Browser/renderers/index.js"},{"key":"components/Viewer/index.js"},{"key":"next.config.js"},{"key":"package-lock.json"},{"key":"package.json"},{"key":"pages/[...slug].js"},{"key":"pages/_app.js"},{"key":"pages/api/file/[...slug].js"},{"key":"pages/api/index.js"},{"key":"pages/api/tree/[...slug].js"},{"key":"pages/index.js"},{"key":"pages/old/index.js"},{"key":"public/favicon.ico"},{"key":"public/vercel.svg"},{"key":"styles/globals.css"},{"key":"styles/old.module.css"},{"key":"utils/api.js"},{"key":"yarn.lock"}];
+
 export default function Home() {
     const router = useRouter();
     const { slug } = router.query;
@@ -39,7 +41,8 @@ export default function Home() {
         <div className="container mt-3">
             <div className="row">
                 <div className="col-2">
-                    {filesError ? (
+                    <Browser files={mockTree} />
+                    {/*filesError ? (
                         <h1>ERROR</h1>
                     ) : filesLoading ? (
                         <div className="spinner-border" role="status">
@@ -47,7 +50,7 @@ export default function Home() {
                         </div>
                     ) : (
                         <Browser files={files} handleFileClick={handleFileClick} />
-                    )}
+                    )*/}
                 </div>
                 <div className="col">
                     {fileLoading ? (
