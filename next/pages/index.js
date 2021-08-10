bd1ee037d21e689333c645a3106cd3590bd49c47
Justin Chang <justinpchang>
2021-08-10 08:17:35 -0400

    Add basic home page

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Home</h1>;
+    return (
+        <div className="container position-relative">
+            <h1 className="display-3 text-center mt-5">
+                Welcome to GithubView
+            </h1>
+            <p className="lead text-center mt-5">
+                Navigate to a GitHub repo and replace the base URL with{' '}
+                <pre>githubview.com</pre>
+            </p>
+        </div>
+    );
 }
