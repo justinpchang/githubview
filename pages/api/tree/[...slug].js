ada5f6577609c8a195d0e65c381ee6b8b7bd4047
Justin Chang <justin.p.chang@gmail.com>
2022-08-04 12:00:14 -0400

    Error out on 401 correctly

--- pages/api/tree/[...slug].js
+++ pages/api/tree/[...slug].js
@@ -8,21 +8,35 @@ export default async function handler(req, res) {
         const session = await getSession({ req });
         if (!session?.username || !session?.accessToken) {
             res.status(401).send('not logged in');
+            return;
         }
 
         // Get data from Github API
-        const token = Buffer.from(`${session.username}:${session.accessToken}`, 'utf8').toString('base64');
-        const commits = (await axios.get(`https://api.github.com/repos/${user}/${repo}/commits`, {
-            headers: {
-                'Authorization': `Basic ${token}`,
-            },
-        })).data;
+        const token = Buffer.from(
+            `${session.username}:${session.accessToken}`,
+            'utf8'
+        ).toString('base64');
+        const commits = (
+            await axios.get(
+                `https://api.github.com/repos/${user}/${repo}/commits`,
+                {
+                    headers: {
+                        Authorization: `Basic ${token}`,
+                    },
+                }
+            )
+        ).data;
         const sha = commits[0].sha;
-        const tree = (await axios.get(`https://api.github.com/repos/${user}/${repo}/git/trees/${sha}?recursive=true`, {
-            headers: {
-                'Authorization': `Basic ${token}`,
-            },
-        })).data.tree;
+        const tree = (
+            await axios.get(
+                `https://api.github.com/repos/${user}/${repo}/git/trees/${sha}?recursive=true`,
+                {
+                    headers: {
+                        Authorization: `Basic ${token}`,
+                    },
+                }
+            )
+        ).data.tree;
 
         let obj = { files: [] };
         for (const file of tree) {
